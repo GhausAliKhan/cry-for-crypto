923bc069aa51514ff3b96dce34ab6371a9a38226
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-10-05 16:34:49 +0500

    Add wrapper to coin chart Details

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -3,7 +3,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { Chart, registerables } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { fetchCryptoDetails, fetchCryptoHistoricalData } from '../redux/cryptoSlice';
+import {
+  fetchCryptoDetails,
+  fetchCryptoHistoricalData,
+} from '../redux/cryptoSlice';
+import '../styles/cryptodetails.css';
 
 const CryptoDetails = () => {
   const dispatch = useDispatch();
@@ -48,52 +52,54 @@ const CryptoDetails = () => {
           'Loading Chart...'
         )}
       </div>
-      <h3>
-        <strong>Name:</strong>
-        <span>{details ? details.name : 'Loading...'}</span>
-      </h3>
-      <p>
-        <strong>Symbol:</strong>
-        <span>{details ? details.symbol : 'Loading...'}</span>
-      </p>
-      <p>
-        <strong>Price (USD):</strong>
-        <span>
-          {details
-            ? parseFloat(details.priceUsd).toLocaleString('en-US', {
-              style: 'currency',
-              currency: 'USD',
-              minimumFractionDigits: 2,
-            })
-            : 'Loading...'}
-        </span>
-      </p>
-      <p>
-        <strong>Rank:</strong>
-        <span>{details ? details.rank : 'Loading...'}</span>
-      </p>
-      <p>
-        <strong>Market Cap (USD):</strong>
-        <span>
-          {details
-            ? parseFloat(details.marketCapUsd).toLocaleString('en-US', {
-              style: 'currency',
-              currency: 'USD',
-            })
-            : 'Loading...'}
-        </span>
-      </p>
-      <p>
-        <strong>24hr Volume (USD):</strong>
-        <span>
-          {details
-            ? parseFloat(details.volumeUsd24Hr).toLocaleString('en-US', {
-              style: 'currency',
-              currency: 'USD',
-            })
-            : 'Loading...'}
-        </span>
-      </p>
+      <div className="chartDetail">
+        <h3>
+          <strong>Name:</strong>
+          <span>{details ? details.name : 'Loading...'}</span>
+        </h3>
+        <p>
+          <strong>Symbol:</strong>
+          <span>{details ? details.symbol : 'Loading...'}</span>
+        </p>
+        <p>
+          <strong>Price (USD):</strong>
+          <span>
+            {details
+              ? parseFloat(details.priceUsd).toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD',
+                minimumFractionDigits: 2,
+              })
+              : 'Loading...'}
+          </span>
+        </p>
+        <p>
+          <strong>Rank:</strong>
+          <span>{details ? details.rank : 'Loading...'}</span>
+        </p>
+        <p>
+          <strong>Market Cap (USD):</strong>
+          <span>
+            {details
+              ? parseFloat(details.marketCapUsd).toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD',
+              })
+              : 'Loading...'}
+          </span>
+        </p>
+        <p>
+          <strong>24hr Volume (USD):</strong>
+          <span>
+            {details
+              ? parseFloat(details.volumeUsd24Hr).toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD',
+              })
+              : 'Loading...'}
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
